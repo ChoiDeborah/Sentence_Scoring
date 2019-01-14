ddc7afeaceaf939091edf7ef901fe171d032cb92
ChoiDeborah <adorablejimini@gmail.com>
2019-01-14 15:56:11 +0900

    Update 전치사-to

--- src/search_function.py
+++ src/search_function.py
@@ -12,7 +12,7 @@ def Load_XML(pathName):
     while True:
         line = f.readline()                 # 파일에서 한 라인을 읽어 라인에 저장
         
-        if not line and not isline:         # 만약 라인이 NULL 값이면서 이전 라인 존재시 브래이크
+        if not line and not isline:         # 만약 라인이 NULL 값이면서 이전 라인 존재 시 브래이크
             break
 
         if isLine(line):                    # 라인이 줄바꿈, 주석, 공백에 해당하지 않으면
@@ -38,11 +38,14 @@ def parse_String_To_XML(string , count):    # string -> XML
     
     root = ET.fromstring(string)            # string을 root에 넣고 파싱한다
     count += 1                              # 문장 수 + 1 
-    if searching(root, "BE-120"):
-        print("BE-120\n")
-        print(string)
+
+    type = "전치사-to"
+
+    if searching(root, type):
+        print( type + "\n\n" + string)
     else :
         print("False\n")    
+
     return count
 
 # ----------------------------------------
@@ -62,11 +65,11 @@ def searching(node , type):
         for part in node.findall("part"):
     
             if not result :                                 #result 가 False 일 때
-                result = part_search(part, "prd", "be", 1, False, type , "")
+                result = part_search(part, "prd", "be", 1, 0, type , "")
               
             else:                                           #result가 True 가 되면 
                 result = False
-                result = part_search(part, "cpm", "nn", 1, False, type, "")
+                result = part_search(part, "cpm", "nn", 1, 0, type, "")
                 if result:
                     return True
                     
@@ -74,25 +77,31 @@ def searching(node , type):
     if type == "BE-120":
         for part in node.findall("part"):
             if not result:
-                result = part_search(part, "prd", "be" , 1,  False, type, "")
+                result = part_search(part, "prd", "be" , 1,  0, type, "")
 
             else:
                 result = False
-                result = part_search(part, "cpm", "", 1, True, type, "")  # çhunk가 존재하는 경우
+                result = part_search(part, "cpm", "", 1, 1, type, "")  # chunk가 존재하는 경우
                 if result:
                     return True
                     
                     
-
     if type == "VB-100":
         for part in node.findall("part"):
             if not result:
-                result = part_search(part, "prd", "vb", 1, False, type, "")
+                result = part_search(part, "prd", "vb", 1, 0, type, "")
             else:
                 result = False
-                result = part_search(part, "obj", "nn", 1, True, type, "")
+                result = part_search(part, "obj", "nn", 1, 1, type, "")
                 if result:
                     return True
+
+    if type == "전치사-to":
+        for part in node.findall("part"):
+            if not result:
+                result = part_search(part, "", "", 1, 1, type, "")
+                if(result):
+                    return True                   
 
 
     return False
@@ -103,31 +112,40 @@ def part_search(part, role_keyword, pos_keyword, depth, isChunk, type, text):
     result = False
 
     role = part.get("role")                     # if 파트의 롤과 매개변수로 받은 롤을 비교
+
+    if isChunk == 2:                            # word 나 chunk나 상관 없을 때 
+        if role == role_keyword:
+            return True
     
-    if not isChunk:                             # word 비교 시 
-        for chunk in part.findall("chunk"):     # 청크가 존재 하면     
+    if isChunk == 0:                             # word 비교 시 
+        for chunk in part.findall("chunk"):     # chunk가 존재 하면     
             return False                        # False 반환
-        if(role == role_keyword):
+        if(role == role_keyword or role_keyword == ""):
             for word in part.findall("word"):
                 pos = word.get("pos")
-                if pos == pos_keyword or pos_keyword == "":         # 같으면 word의 pos 비교
-                    if word.text == text or text == "":                  # word 의 text 가 ""이거나 같을 때 
-                       return True
+                if pos == pos_keyword or pos_keyword == "":             # 같으면 word의 pos 비교
+                    if text == "":                 # word 의 text 가 ""이거나 같을 때 
+                        return True
+                    elif word.text == text:
+                        return True    
 
-        elif(role_keyword[:1] == "!"):                              # role_keyword 만 제외하고
-            roleTemp = role_keyword[1:]                             # !제외한 나머지 문자열 슬라이싱
+        elif(role_keyword[:1] == "!"):                                  # role_keyword 만 제외하고
+            roleTemp = role_keyword[1:]                                 # !제외한 나머지 문자열 슬라이싱
             if roleTemp == "obj":
-                if role != roleTemp:                                # roleTemp 제외한 나머지 일 경우
+                if role != roleTemp:                                    # roleTemp 제외한 나머지 일 경우
                     for word in part.findall("word"):
                         pos = word.get("pos")
-                        if pos == pos_keyword or pos_keyword == "":         # 같으면 word의 pos 비교
-                            if word.text == text or text == "":             # word 의 text 가 ""이거나 같을 때 
+                        if pos == pos_keyword or pos_keyword == "":     # 같으면 word의 pos 비교
+                            if text == "":                 # word 의 text 가 ""이거나 같을 때 
                                 return True
+                            elif word.text == text:
+                                return True    
 
-    else:                                       # Chunk 비교 시
+
+    if isChunk == 1:                                       # Chunk 비교 시
         for word in part.findall("word"):       # word 가 존재하면
             return False                        # False 반환
-        if(role == role_keyword):
+        if(role == role_keyword or role_keyword == ""):
             for chunk in part.findall("chunk"):
                 pos = chunk.get("pos")
                 if pos == pos_keyword or pos_keyword == "":
@@ -136,8 +154,12 @@ def part_search(part, role_keyword, pos_keyword, depth, isChunk, type, text):
                         pos_keyword = ""
                     if(type == "VB-100"):
                         role_keyword = "trg"
-                        pos_keyword = "cj"    
-                    result = chunk_search(chunk, depth, role_keyword, pos_keyword, type,"")
+                        pos_keyword = "cj"
+                    if(type == "전치사-to"):
+                        role_keyword = "trg"
+                        pos_keyword = ""
+                        text = " to "
+                    result = chunk_search(chunk, depth, role_keyword, pos_keyword, type, text)
                     if result:
                         return True
 
@@ -151,16 +173,28 @@ def chunk_search(chunk, depth, role_keyword, pos_keyword, type, text):
     if type == "BE-120" :
         for part in chunk.findall("part"):
             if not result:
-                result = part_search(part, role_keyword, pos_keyword, depth+1, False, type, " to ")
-            # word.text가 "to" 일 때 참
-            if result:
+                result = part_search(part, role_keyword, pos_keyword, depth+1, 0, type, " to ")     # word.text가 " to " 일 때 참
+            
+            else:
                 result = False
-                result = part_search(part, "!obj", "", depth+1, False, type, "")        #role이 obj가 아닌 것 만
+                result = part_search(part, "!obj", "", depth+1, 0, type, "")                        #role이 obj가 아닌 것 만
                 if result:
                     return True
+
+    if type == "전치사-to":
+        for part in chunk.findall("part"):
+            if not result:        
+               result = part_search(part, role_keyword, pos_keyword, depth+1, 0, type, " to ")
+            else :
+                result = False
+                result = part_search(part, "obj", "", depth+1, 2, type, "")
+                if result :
+                    return True   
+               
+
     else:
         for part in chunk.findall("part"):
-            result = part_search(part, role_keyword, pos_keyword, depth+1, False, type,"")
+            result = part_search(part, role_keyword, pos_keyword, depth+1, 0, type, text)
             if result:
                 return True
 
